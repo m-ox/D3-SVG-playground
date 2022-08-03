2fea653a29995084d15432a3caa6ee329febad6e
Maud <maudlin.oxalis@gmail.com>
2022-08-03 12:15:00 -0400

    completed smiley face

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import { useState } from 'react'
+import { arc } from 'd3'
 
 function App() {
   const [height, setHeight] = useState(window.innerHeight)
@@ -9,28 +10,37 @@ function App() {
   const [eyeOffsetX, setEyeOffsetX] = useState(180)
   const [eyeOffsetY, setEyeOffsetY] = useState(150)
   const [eyeRadius, setEyeRadius] = useState(50)
+  const [ mouthWidth, setMouthWidth] = useState(20)
+  const [ mouthRadius, setMouthRadius] = useState(250)
+
+  const mouthArc = arc()
+    .innerRadius(mouthRadius)
+    .outerRadius(mouthRadius + mouthWidth)
+    .startAngle(Math.PI / 2)
+    .endAngle(Math.PI * 3/2)
 
   return (
     <div className="App">
       <svg width={width} height={height}>
-        <circle
-          cx={centerX}
-          cy={centerY}
-          r={centerY - strokeWidth / 2}
-          fill="yellow"
-          stroke="black"
-          stroke-width={strokeWidth}
-        />
-        <circle
-          cx={centerX - eyeOffsetX}
-          cy={centerY - eyeOffsetY}
-          r={eyeRadius}
-        />
-        <circle
-          cx={centerX + eyeOffsetX}
-          cy={centerY - eyeOffsetY}
-          r={eyeRadius}
-        />
+        <g transform={`translate(${centerX},${centerY})`}>
+          <circle
+            r={centerY - strokeWidth / 2}
+            fill="yellow"
+            stroke="black"
+            stroke-width={strokeWidth}
+          />
+          <circle
+            cx={-eyeOffsetX}
+            cy={-eyeOffsetY}
+            r={eyeRadius}
+          />
+          <circle
+            cx={eyeOffsetX}
+            cy={-eyeOffsetY}
+            r={eyeRadius}
+          />
+          <path d={mouthArc()} />
+        </g>
       </svg>
     </div>
   )
